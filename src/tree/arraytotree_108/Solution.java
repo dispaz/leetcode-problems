105610a46b49739d1ceaa58df4f1bae054991c63
dispaz <vladvoloshyn2@gmail.com>
2020-12-15 06:45:08 +0100

    added 108

---
+++ src/tree/arraytotree_108/Solution.java
@@ -0,0 +1,22 @@
+package tree.arraytotree_108;
+
+import tree.TreeNode;
+
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return recursionArrayToTree(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode recursionArrayToTree(int [] nums, int f, int l){
+        if(f > l){
+            return null;
+        }
+
+        int index = ( f + l ) / 2;
+        TreeNode n = new TreeNode(nums[index]);
+        n.left = recursionArrayToTree(nums, f, index - 1);
+        n.right = recursionArrayToTree(nums, index + 1, l);
+        return n;
+    }
+
+}

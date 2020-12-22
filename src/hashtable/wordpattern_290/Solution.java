17d01029ed6e45f84612ce7a49a71345411efd6a
dispaz <vladvoloshyn2@gmail.com>
2020-12-22 17:40:45 +0100

    added 290

---
+++ src/hashtable/wordpattern_290/Solution.java
@@ -0,0 +1,29 @@
+package hashtable.wordpattern_290;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+        if(pattern.length() != arr.length){
+            return false;
+        }
+        HashMap mapP = new HashMap();
+
+        for(Integer i = 0; i < pattern.length(); i++){
+            if(!mapP.containsKey(pattern.charAt(i))){
+                mapP.put(pattern.charAt(i), i);
+            }
+
+            if(!mapP.containsKey(arr[i])){
+                mapP.put(arr[i], i);
+            }
+
+            if(mapP.get(arr[i]) != mapP.get(pattern.charAt(i)))
+                return false;
+        }
+
+        return true;
+    }
+}

cb8f025072fc0e9073201ac96e6dac6656ff927d
dispaz <vladvoloshyn2@gmail.com>
2020-12-30 17:35:57 +0100

    added 1642

---
+++ src/heap/buildingtoreach_1642/Solution.java
@@ -0,0 +1,22 @@
+package heap.buildingtoreach_1642;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int i = 0 ; i < heights.length - 1; i++){
+            int d = heights[i + 1] - heights[i];
+            if(d > 0){
+                heap.add(d);
+            }
+            if(heap.size() > ladders){
+                bricks -= heap.poll();
+            }
+            if(bricks < 0){
+                return i;
+            }
+        }
+        return heights.length - 1;
+    }
+}

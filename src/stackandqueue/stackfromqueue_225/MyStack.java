3b726d0835f0d0575f91ad47ab0d6bb5a21e7054
dispaz <vladvoloshyn2@gmail.com>
2021-01-04 17:30:51 +0100

    review 225

--- src/stackandqueue/stackfromqueue_225/MyStack.java
+++ src/stackandqueue/stackfromqueue_225/MyStack.java
@@ -5,9 +5,11 @@ import java.util.Queue;
 
 public class MyStack {
     Queue<Integer> q;
+    Queue<Integer> q2;
     /** Initialize your data structure here. */
     public MyStack() {
         q = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
 
     /** Push element x onto stack. */
@@ -18,6 +20,16 @@ public class MyStack {
             q.add(q.remove());
             size--;
         }
+    }
+
+    public void push2(int x){
+        q2.add(x);
+        while(!q.isEmpty()){
+            q2.add(q.remove());
+        }
+        Queue<Integer> temp = q;
+        q = q2;
+        q2 = temp;
     }
 
     /** Removes the element on top of the stack and returns that element. */

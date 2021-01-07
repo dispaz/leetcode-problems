ba3f60f00ee7eb2d2ad086496126b07cdc77bf8c
dispaz <vladvoloshyn2@gmail.com>
2021-01-07 12:37:28 +0100

    added 70

---
+++ src/dynamicprogramming/climbstairs_70/Solution.java
@@ -0,0 +1,23 @@
+package dynamicprogramming.climbstairs_70;
+
+public class Solution {
+    // dynamic
+    public int climbStairs(int n) {
+        int [] arr = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for(int i = 2; i < arr.length; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
+
+    //brute force
+    public int climbStairsBrute(int n ){
+        if(n < 2){
+            return 1;
+        }
+        return climbStairsBrute(n - 1) + climbStairsBrute(n - 2);
+    }
+
+}

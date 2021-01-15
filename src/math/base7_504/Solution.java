dc35b34fe4feb729897af030e9151b48e1a14ec0
dispaz <vladvoloshyn2@gmail.com>
2021-01-15 08:12:40 +0100

    added 504

---
+++ src/math/base7_504/Solution.java
@@ -0,0 +1,18 @@
+package math.base7_504;
+
+public class Solution {
+    public String convertToBase7(int num) {
+        if(num == 0) return "0";
+        StringBuilder result = new StringBuilder();
+        boolean minus = num < 0;
+        if(minus){
+            num = -num;
+        }
+        while(num != 0){
+            result.insert(0, num % 7);
+            num = num / 7;
+        }
+        if(minus) result.insert(0, '-');
+        return result.toString();
+    }
+}

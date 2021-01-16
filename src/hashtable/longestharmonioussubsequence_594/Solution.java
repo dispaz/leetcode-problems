3c3a450f309fc039cb60faeae594f57d094fa0d5
dispaz <vladvoloshyn2@gmail.com>
2021-01-16 11:07:42 +0100

    594

---
+++ src/hashtable/longestharmonioussubsequence_594/Solution.java
@@ -0,0 +1,23 @@
+package hashtable.longestharmonioussubsequence_594;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int max = 0;
+
+        for(int key : map.keySet()){
+            if(map.containsKey(key + 1)){
+                max = Math.max(max, map.get(key) + map.get(key + 1));
+            }
+        }
+
+        return max;
+    }
+}

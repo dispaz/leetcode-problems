e3d6ce2044c794a7a0e60d70f328f09529fa1881
dispaz <vladvoloshyn2@gmail.com>
2021-01-25 17:17:56 +0100

    added 993

---
+++ src/tree/cousinsbinarytree_993/Solution.java
@@ -0,0 +1,30 @@
+package tree.cousinsbinarytree_993;
+
+import tree.TreeNode;
+
+public class Solution {
+    TreeNode l = null;
+    TreeNode r = null;
+    int leftDepth = -1;
+    int rightDepth = -2;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+
+        getDepth(root, null, x, y, 0);
+        return l != r && leftDepth == rightDepth;
+    }
+
+    public void getDepth(TreeNode n, TreeNode parent, int x, int y, int level){
+        if(n == null) return;
+        if(n.val == x) {
+            l = parent;
+            leftDepth = level;
+        } else if(n.val == y) {
+            r = parent;
+            rightDepth = level;
+        } else {
+            getDepth(n.left, n, x, y, level + 1);
+            getDepth(n.right, n, x, y, level + 1);
+        }
+    }
+}

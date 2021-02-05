62140ede11871f98ae37fe18a411d23fa3ffb16f
dispaz <vladvoloshyn2@gmail.com>
2021-02-05 08:09:49 +0100

    added 441

---
+++ src/math/arrangingcoins_441/Solution.java
@@ -0,0 +1,15 @@
+package math.arrangingcoins_441;
+
+public class Solution {
+    public int arrangeCoins(int n) {
+        int dots = 1;
+        double val = 1.0;
+        while(val * dots < n){
+            dots++;
+            val += 0.5f;
+        }
+        System.out.println(dots + " " + val);
+        if(val * dots == n) return dots;
+        else return dots - 1;
+    }
+}

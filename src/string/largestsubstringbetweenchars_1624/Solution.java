c1f07caa115257c5368541abf86858831b552627
dispaz <vladvoloshyn2@gmail.com>
2021-03-16 07:54:38 +0100

    added 1624

---
+++ src/string/largestsubstringbetweenchars_1624/Solution.java
@@ -0,0 +1,27 @@
+package string.largestsubstringbetweenchars_1624;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        HashMap<Character, Integer> map = new HashMap();
+        int max = -1;
+        for(int i = 0; i < s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), i + 1);
+            }
+        }
+
+        if(map.size() == s.length()) return max;
+
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(max > i){
+                break;
+            }
+            if(max < i - map.get(s.charAt(i))){
+                max = i - map.get(s.charAt(i));
+            }
+        }
+        return max;
+    }
+}

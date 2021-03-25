b7731cb014b7eb205aa8cfd8795d8e3da9089d9f
dispaz <vladvoloshyn2@gmail.com>
2021-03-25 07:40:44 +0100

    added 744

---
+++ src/binarysearch/findsmallestletter_744/Solution.java
@@ -0,0 +1,22 @@
+package binarysearch.findsmallestletter_744;
+
+public class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0, end = letters.length - 1, mid = letters.length / 2;
+        char res = letters[0];
+
+        while(start <= end){
+            if(letters[mid] <= target){
+                start = mid + 1;
+            }
+            else if(letters[mid] > target){
+                end = mid - 1;
+                res = letters[mid];
+            }
+
+            mid = (start + end) / 2;
+        }
+
+        return res;
+    }
+}

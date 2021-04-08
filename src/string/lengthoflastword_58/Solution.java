ea4b4d7ca1df48c479a8f3acc99ae7760e874713
dispaz <vladvoloshyn2@gmail.com>
2021-04-08 07:10:21 +0200

    added 58

---
+++ src/string/lengthoflastword_58/Solution.java
@@ -0,0 +1,16 @@
+package string.lengthoflastword_58;
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) == ' ' && count > 0)
+                break;
+
+            count += s.charAt(i) != ' ' ? 1 : 0;
+        }
+
+        return count;
+    }
+}

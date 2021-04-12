b6b7048960abeadc773c5f31b33341017e64c4ab
dispaz <vladvoloshyn2@gmail.com>
2021-04-12 07:24:32 +0200

    added 1539

---
+++ src/arrayproblems/kthpositivemissingnumber_1539/Solution.java
@@ -0,0 +1,22 @@
+package arrayproblems.kthpositivemissingnumber_1539;
+
+public class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        if(arr[0] > k)
+            return k;
+
+        k -= (arr[0] - 1);
+
+        for(int i = 1; i < arr.length && k > 0; i++){
+            int diff = arr[i] - arr[i - 1] - 1;
+            if(diff > 0){
+                if(k - diff <= 0)
+                    return arr[i - 1] + k;
+
+                k -= diff;
+            }
+        }
+
+        return arr[arr.length - 1] + k;
+    }
+}

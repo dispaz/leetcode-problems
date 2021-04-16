f8296d0efe509da9374c37c4508a3904a48eacaa
dispaz <vladvoloshyn2@gmail.com>
2021-04-16 06:55:55 +0200

    added 1079

---
+++ src/backtracking/lettertilepossibilities_1079/Solution.java
@@ -0,0 +1,24 @@
+package backtracking.lettertilepossibilities_1079;
+
+public class Solution {
+    public int numTilePossibilities(String tiles) {
+        int [] count = new int[26];
+        for(char c : tiles.toCharArray()){
+            count[c - 'A']++;
+        }
+        return dfs(count);
+    }
+
+    private int dfs(int [] arr){
+        int sum = 0;
+        for(int i = 0; i < 26; i++){
+            if(arr[i] != 0){
+                sum++;
+                arr[i]--;
+                sum += dfs(arr);
+                arr[i]++;
+            }
+        }
+        return sum;
+    }
+}

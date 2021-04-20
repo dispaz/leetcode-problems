2d951ca6d6a48072828dc8840bd5fe11dc32a1e2
dispaz <vladvoloshyn2@gmail.com>
2021-04-20 07:51:40 +0200

    added 1128

---
+++ src/arrayproblems/numberofequivalentdominopairs_1128/Solution.java
@@ -0,0 +1,19 @@
+package arrayproblems.numberofequivalentdominopairs_1128;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int[] d : dominoes){
+            int k = Math.min(d[0], d[1]) * 10 + Math.max(d[0],d[1]);
+            map.put(k, map.getOrDefault(k, 0) + 1);
+        }
+
+        int res = 0;
+        for(int v : map.values()){
+            res += v * (v - 1) / 2;
+        }
+        return res;
+    }
+}

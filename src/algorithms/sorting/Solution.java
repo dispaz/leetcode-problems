4373e0f061a05672d1f8085888934ec379b19eaa
dispaz <vladvoloshyn2@gmail.com>
2021-04-22 19:56:07 +0200

    added merge sort

--- src/algorithms/sorting/Solution.java
+++ src/algorithms/sorting/Solution.java
@@ -2,48 +2,15 @@ package algorithms.sorting;
 
 import java.util.Random;
 
+import static algorithms.sorting.merge.MergeSort.mergeSort;
+import static algorithms.sorting.quick.QuickSort.quickSort;
+
 public class Solution {
-    private static Random rand = new Random();
     public static void main(String [] args){
-        int [] a = new int[] {4, 9, 6, 1, 10, 5, 2, 7, 8, 3};
+        int [] a = new int[] {4, 9, 6, 1, 10, 5, 2, 7, 8, 3, 11, 16, 13, 12, 14, 19, 15, 20, 17, 18};
         printArr(a);
-        quickSort(a);
+        mergeSort(a);
         printArr(a);
-    }
-
-    private static void quickSort(int [] a){
-        reccursionQuickSort(a, 0, a.length - 1);
-    }
-
-    private static void reccursionQuickSort(int [] a, int start, int end){
-        if(start < end){
-            int pi = partition_r(a, start, end);
-            reccursionQuickSort(a, start, pi - 1);
-            reccursionQuickSort(a, pi + 1, end);
-        }
-    }
-    private static int partition_r(int [] a, int start, int end){
-        int pivot = rand.nextInt(end - start) + start;
-        swap(a, pivot, end);
-        return partition(a, start, end);
-    }
-    private static int partition(int[] arr, int start, int end){
-        int pivot = arr[end];
-
-        int i = start;
-        for(int j = start; j < end; j++){
-            if(arr[j] < pivot){
-                swap(arr, i++, j);
-            }
-        }
-        swap(arr, i, end);
-        return i;
-    }
-
-    private static void swap(int [] arr, int first, int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
     }
 
     private static void printArr(int [] arr){

6f040ca661de3e55146be81d5e9c3151311c4a88
dispaz <vladvoloshyn2@gmail.com>
2021-05-16 15:02:42 +0200

    added 876

---
+++ src/problems/linkedlist/middleoflinkedlist_876/Solution.java
@@ -0,0 +1,15 @@
+package problems.linkedlist.middleoflinkedlist_876;
+
+import problems.linkedlist.ListNode;
+
+public class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
